86aae3e32551d03f4d2f0cd3ee9ede83256e388f
yifeng <gengyifeng@gmail.com>
2014-01-13 19:46:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+ssh to a host through http proxy using corkscrew.
+ssh_proxy.py is used to translate hostname to ip for corkscrew. 
---
+++ ssh_proxy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+import sys
+from ipaddr import IPAddress
+def parse_host(host):
+    try:
+        IPAddress(host)
+        return host
+    except ValueError:
+        f=open('/etc/hosts')
+        lines=f.readlines()
+        f.close()
+        for line in lines:
+            if line[0]=='#':
+                continue
+            tokens=line.split()
+            if tokens[1]==host:
+                return tokens[0]
+            if len(tokens)>2 and tokens[2]==host:
+                return token
+def run():
+    cmd='corkscrew '+" ".join(sys.argv[1:3])+" "+parse_host(sys.argv[3])+" "+" ".join(sys.argv[4:])
+#    print cmd
+    os.system(cmd);
+if __name__=='__main__':
+    run();
